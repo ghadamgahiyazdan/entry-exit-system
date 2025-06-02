20e4a6967ab9d3b516bf6ae5d2fe6b462aa45d9b
ghadamgahiyazdan <ghadamgahiyazdan@gmail.com>
2025-06-02 16:17:14 +0330

    little change

---
+++ prisma/migrations/20250602124653_first/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `hours` on the `Report` table. All the data in the column will be lost.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Report" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "status" TEXT NOT NULL,
+    "employeeId" INTEGER NOT NULL,
+    "shiftId" INTEGER NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Report_employeeId_fkey" FOREIGN KEY ("employeeId") REFERENCES "Employee" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Report_shiftId_fkey" FOREIGN KEY ("shiftId") REFERENCES "Shift" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Report" ("createdAt", "employeeId", "id", "shiftId", "status", "updatedAt") SELECT "createdAt", "employeeId", "id", "shiftId", "status", "updatedAt" FROM "Report";
+DROP TABLE "Report";
+ALTER TABLE "new_Report" RENAME TO "Report";
+CREATE INDEX "Report_employeeId_shiftId_idx" ON "Report"("employeeId", "shiftId");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
