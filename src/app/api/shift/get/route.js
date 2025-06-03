860bbc276db40e5ba2184f879ce9707d9f9e367f
ghadamgahiyazdan <ghadamgahiyazdan@gmail.com>
2025-06-03 12:49:03 +0330

    add get employee and shift

---
+++ src/app/api/shift/get/route.js
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function GET() {
+  try {
+    const data = await prisma.shift.findMany();
+    return NextResponse.json({ data }, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching employees:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch employees" },
+      { status: 500 }
+    );
+  }
+}
