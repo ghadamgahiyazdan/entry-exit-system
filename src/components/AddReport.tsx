0d1ef088fa50959e6d670adb5ebfa80ba429d4f3
ghadamgahiyazdan <ghadamgahiyazdan@gmail.com>
2025-06-04 16:03:17 +0330

    fix add shift front

--- src/components/AddReport.tsx
+++ src/components/AddReport.tsx
@@ -1,36 +1,64 @@
 import React from 'react';
-import { Input } from './ui/input';
-import { Label } from './ui/label';
 import { Button } from './ui/button';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
 const AddReport = () => {
-    return (
-      <section className="col-span-3 lg:col-span-1 bg-gray-100 flex flex-col justify-center items-center p-6 gap-4 text-2xl border rounded-lg">
-        <h1 className="text-center">اضافه کردن گزارش</h1>
-        <form className="flex flex-col gap-3 w-full">
-          <div className="flex items-center gap-2">
-            <Input id="reportEmployeeName" name="reportEmployeeName" className='bg-white flex-1' />
-            <Label htmlFor="reportEmployeeName" className="text-right whitespace-nowrap">
-              نام کارمند
-            </Label>
-          </div>
+  return (
+    <section className="col-span-3 lg:col-span-1 bg-gray-100 flex flex-col justify-center items-center p-6 gap-4 text-2xl border rounded-lg">
+      <h1 className="text-center">اضافه کردن گزارش</h1>
+      <form className="flex flex-col justify-center items-center gap-3 w-full">
+        <div className="flex items-center gap-2">
+          {/*employee*/}
+          <Select>
+            <SelectTrigger className="w-[180px] bg-white">
+              <SelectValue placeholder="کارمند" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="employee1">کارمند 1</SelectItem>
+              <SelectItem value="employee2">کارمند 2</SelectItem>
+              <SelectItem value="employee3">کارمند 3</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
 
-          <div className="flex items-center gap-2">
-            <Input id="reportShift" name="reportShift" className='bg-white flex-1' />
-            <Label htmlFor="reportShift" className="text-right whitespace-nowrap">
-              شیفت
-            </Label>
-          </div>
+        <div className="flex items-center gap-2">
+          {/*shift*/}
+          <Select>
+            <SelectTrigger className="w-[180px]  bg-white">
+              <SelectValue placeholder="شیفت" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="morning">صبح</SelectItem>
+              <SelectItem value="afternoon">عصر</SelectItem>
+              <SelectItem value="night">شب</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
 
-          <div className="flex items-center gap-2">
-            <Input id="reportEmployeeName2" name="reportEmployeeName2" className='bg-white flex-1' />
-            <Label htmlFor="reportEmployeeName2" className="text-right whitespace-nowrap">
-              نام کارمند
-            </Label>
-          </div>
-        </form>
-        <Button className='w-10'>تایید</Button>
-      </section>
-    );
+        <div className="flex items-center gap-2">
+          {/*status*/}
+          <Select>
+            <SelectTrigger className="w-[180px] bg-white">
+              <SelectValue placeholder="وضعیت" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="تأخیر">تأخیر</SelectItem>
+              <SelectItem value="مرخصی">مرخصی</SelectItem>
+              <SelectItem value="غایب">غایب</SelectItem>
+              <SelectItem value="حاضر">حاضر</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+      </form>
+      <Button className='w-10'>تایید</Button>
+    </section>
+  );
 };
 
 export default AddReport;
